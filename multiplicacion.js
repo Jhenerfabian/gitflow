3660bc7c177502c5d71a6deda4622de614ebcc7b
Jhenerfabian <fabianabcvargas01@gmail.com>
2026-03-10 18:45:24 -0500

    multiplicacion

---
+++ multiplicacion.js
@@ -0,0 +1,9 @@
+// Definimos dos números
+let numero1 = 6;
+let numero2 = 5;
+
+// Realizamos la multiplicación
+let resultado = numero1 * numero2;
+
+// Mostramos el resultado en consola
+console.log("El resultado de la multiplicación es:", resultado);
